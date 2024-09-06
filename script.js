b4fb3dea301049dc7be199f78a76710ed085045e
okechpj <okechpj@gmail.com>
2024-09-06 07:07:41 +0300

    added a js file

---
+++ script.js
@@ -0,0 +1,7 @@
+// Display a greeting message in the console
+console.log("Hello, World! This message is from the JavaScript file.");
+
+// Optionally, add a pop-up alert
+window.onload = function() {
+    alert("Welcome to the Hello World Web Application!");
+};
